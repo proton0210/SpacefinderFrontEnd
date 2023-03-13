7be5d6dced6bbd6ee6a6c291e4d8ae61b43d2f49
Vidit Shah <vidit0210@protonmail.com>
2023-03-13 21:50:33 +0530

    tailwind ui

---
+++ services/AuthService.ts
@@ -0,0 +1,12 @@
+export class AuthService {
+  public async login(userName: string, password: string): Promise<User | undefined> {
+    if (userName === 'user' && password === '1234') {
+      return {
+        userName: userName,
+        email: 'some@email.com',
+      };
+    } else {
+      return undefined;
+    }
+  }
+}
