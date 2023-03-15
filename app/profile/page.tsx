281802e1410e11d069f0bdef0c5d5496fc18fb47
Vidit Shah <vidit0210@protonmail.com>
2023-03-15 15:10:32 +0530

    To be deleted

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,11 +4,43 @@ import { useUser } from '../UserContext';
 
 type Props = {};
 import { useRouter } from 'next/navigation';
-const Profile = (props: Props) => {
+import Table from './table';
+import { AuthService } from '@/services/AuthService';
+
+// async function getData() {
+//   const result = [];
+//   result.push({
+//     Name: 'description',
+//     Value: 'Best user ever!',
+//   });
+//   result.push({
+//     Name: 'job',
+//     Value: 'Engineer',
+//   });
+//   result.push({
+//     Name: 'age',
+//     Value: '25',
+//   });
+//   result.push({
+//     Name: 'experience',
+//     Value: '3 years',
+//   });
+//   return result;
+// }
+
+const Profile = async (props: Props) => {
   const router = useRouter();
   const { user } = useUser();
   if (!user) return router.push('/login');
-  return <div className="text-center mt-10">Profile page</div>;
+  // const data = await getData();
+  // console.log(data);
+
+  return (
+    <>
+      <div className="text-center mt-10">Welcome </div>
+      {/* <Table /> */}
+    </>
+  );
 };
 
 export default Profile;
